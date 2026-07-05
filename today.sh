5cd167a80373de3b4a5e12b0ebc094f370f3f9d0
Niels Groen <hooded.main@gmail.com>
2026-07-05 17:23:11 +0200

    Forgot to add a git add to actually commit the right files

--- today.sh
+++ today.sh
@@ -2,5 +2,6 @@ python update.py --once
 python parse.py --plain > current.txt
 python parse.py
 read -p "Press enter to continue"
+git add eggs.csv current.txt
 git commit -m "Added today's egg count"
 git push
