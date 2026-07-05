4eaea9cace85b762f8fa44b74354e7a4712a6966
Niels Groen <hooded.main@gmail.com>
2026-07-05 17:16:43 +0200

    Added support for only asking for one date

--- update.py
+++ update.py
@@ -1,11 +1,18 @@
+import sys
 import datetime as dt
+
+max = -1
+if sys.argv[1] == "--once":
+    max = 1
 
 lastDate = dt.date.today()
 
 with open("eggs.csv", "a", newline="") as file:
-    while True:
+    count = 0
+    while count != max:
         date = lastDate.strftime("%d/%m/%Y")
         eggs = input(date + ", ")
         if eggs:
             print(date + ", " + eggs, file=file, flush=True)
         lastDate = lastDate - dt.timedelta(days=1)
+        count += 1
